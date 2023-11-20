e211ace5ff70e4735b35144c4e77ead981efe2d7
Kris10 <kmrichie611@gmail.com>
2023-11-20 12:07:18 -0600

    added GET, POST, & PUT routes for Category

--- models/index.js
+++ models/index.js
@@ -6,24 +6,27 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'category_id'
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
 })
 
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
-  //onDelete: 'CASCADE',
+  
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  foreignKey: 'tag_id',
+  through: ProductTag,
+  foreignKey: 'product_id',
 
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  foreignKey: 'product_id',
+  through: ProductTag,
+  foreignKey: 'tag_id',
 });
 
 module.exports = {
